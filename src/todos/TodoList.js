946eb9b2d845cf1cd5341a246c5543bcb86d1283
Sid0rr0 <elhungus@gmail.com>
2020-07-16 17:32:17 +0200

    displaying the list

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './TodoList.css'
 import TodoListItem from './TodoListItem';
+import NewTodoForm from './NewTodoForm';
 
-const TodoList =  ({ todos }) => (
+const TodoList =  ({ todos = [] }) => (
     <div className="list-wrapper">
+        <NewTodoForm />
         {todos.map(todo => <TodoListItem todo={todo} />)}
     </div>
 )
