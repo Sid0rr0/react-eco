06b3fd2c3072533da0084481ba8f32ac9b225fbe
Sid0rr0 <elhungus@gmail.com>
2020-07-17 23:51:25 +0200

    added persisting store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-    <Provider store={configureStore()} >
-        <App />
+    <Provider store={store} >
+        <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+            <App />
+        </PersistGate>
     </Provider>, 
     document.getElementById('root')
 );
