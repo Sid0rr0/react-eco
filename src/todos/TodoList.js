b496e2124a711c73d04ec8e4187476e2a464fefa
Sid0rr0 <elhungus@gmail.com>
2020-07-22 17:06:03 +0200

    added selectors to state

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,6 +4,7 @@ import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm';
 import { connect } from 'react-redux';
 import { loadTodos, removeTodoRequest, markCompletedTodoRequest } from './thunks';
+import { getTodos, getTodosLoading } from './selectors';
 
 const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
     useEffect(() => {
@@ -22,8 +23,8 @@ const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, 
 };
 
 const mapStateToProps = state => ({
-    isLoading: state.isLoading,
-    todos: state.todos,
+    isLoading: getTodosLoading(state),
+    todos: getTodos(state),
 });
 
 const mapDispatchToProps = dispatch => ({
