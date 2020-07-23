e5ca15bd395aceeaee835fee44337b7eb60991c4
Sid0rr0 <elhungus@gmail.com>
2020-07-23 12:52:58 +0200

    created logic in styled components

---
+++ src/todos/tests/TodoListItem.test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { getBorderStyleForDate } from '../TodoListItem'
+
+describe('getBorderStyleForDate', () => {
+    it('return none when the date is less than five days ago', () => {
+        const today = Date.now();
+        const recentDate = new Date(Date.now() - 86400000 * 3);
+        
+        const expected = 'none';
+        const actual = getBorderStyleForDate(recentDate, today);
+
+        expect(actual).to.equal(expected);
+    });
+
+    it('returns a border when the date is more than 5 days', () => {
+        const today = Date.now();
+        const recentDate = new Date(Date.now() - 86400000 * 6);
+
+        const expected = '2px solid red';
+        const actual = getBorderStyleForDate(recentDate, today);
+
+        expect(actual).to.equal(expected);
+    });
+});
